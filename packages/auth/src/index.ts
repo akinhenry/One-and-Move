c85bb1ff9f8a903b4ed586fca2d4b585de3ab415
Akin Henry <akinhenry00@gmail.com>
2026-03-14 14:14:02 -0500

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,28 @@
+import { expo } from "@better-auth/expo";
+import { db } from "@One-and-Move/db";
+import * as schema from "@One-and-Move/db/schema/auth";
+import { env } from "@One-and-Move/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [
+    env.CORS_ORIGIN,
+    "One-and-Move://",
+    ...(env.NODE_ENV === "development"
+      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+      : []),
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  plugins: [nextCookies(), expo()],
+});
